b87c4d895f6e87c669e3588b84abcc3a497aff1e
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-04-15 14:20:44 +0000

    added import random and game starting message

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+print("----------------------")
+print("Welcome to tobbes Tic Tac Toe game")
+print("The game is played on a grid that's 3 squares by 3 squares.")
+print("Connect three to win the game up, down, across, or diagonally")
+print("if board is full its a draw")
+
+
+print("----------------------")
+
+player = input("Enter Player name: ")
+print("Hello", player + "!")
