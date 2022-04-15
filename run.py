92ad7a171f5e0d68305dfcbc42fcd3505f23abb9
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-04-15 14:23:35 +0000

    added possible numbers and rows/colums on the board

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 
 
-print("----------------------")
+print("----------------------")  # welcome message
 print("Welcome to tobbes Tic Tac Toe game")
 print("The game is played on a grid that's 3 squares by 3 squares.")
 print("Connect three to win the game up, down, across, or diagonally")
@@ -12,3 +12,8 @@ print("----------------------")
 
 player = input("Enter Player name: ")
 print("Hello", player + "!")
+
+possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rows = 3
+cols = 3
