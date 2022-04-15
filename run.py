377d97bbf0de9e5909e44bb39e611868ff3b8b1b
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-04-15 14:26:18 +0000

    code to modify gameboard

--- run.py
+++ run.py
@@ -17,3 +17,16 @@ possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 rows = 3
 cols = 3
+
+
+def printGameBoard():  # makes the gameboard
+    for x in range(rows):
+        print("\n+---+---+---+")
+        print("|", end="")
+        for y in range(cols):
+            print("", board[x][y], end=" |")
+    print("\n+---+---+---+")
+
+
+def modifyArray(num, turn):  # modifies gameboard
+    board[(num-1)//3][(num-1) % 3] = turn
